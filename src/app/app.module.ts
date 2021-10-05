929767931470df0e6d7ea7490c98dbde959d5afc
leovalentino <leonnardovalentino@gmail.com>
2021-10-05 15:09:14 -0300

    Adicionando funcionalidades de autorização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,10 +16,11 @@ import {ShoppingListService} from './shopping-list/shopping-list.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {RecipeGuardService} from './recipes/recipe-guard.service';
 import {AuthComponent} from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
+import {AuthInterceptorService} from './auth/auth-interceptor.service';
 
 @NgModule({
     declarations: [
@@ -44,7 +45,8 @@ import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinne
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [RecipeSerivce, ShoppingListService, RecipeGuardService],
+  providers: [RecipeSerivce, ShoppingListService, RecipeGuardService,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {RecipeSerivce} from '../recipes/recipe.serivce';
 import {Recipe} from '../recipes/recipe.model';
 import {exhaustMap, map, take, tap} from 'rxjs/operators';
@@ -21,11 +21,9 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.authService.userSubject.pipe(
-      take(1),
-      exhaustMap(user => {
-        return this.http.get<Recipe[]>('https://ng-course-recipe-book-7a77b-default-rtdb.firebaseio.com/recipes.json');
-      }),
+    return this.http.get<Recipe[]>(
+      'https://ng-course-recipe-book-7a77b-default-rtdb.firebaseio.com/recipes.json'
+    ).pipe(
       map(recipes => {
         return recipes.map(recipe => {
           return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
